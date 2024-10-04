060f5d776e71df81ee753731c4c0545f17963924
PlitMa <midori17166636@yandex.ru>
2024-10-04 18:27:56 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,10 @@
+name = "Мария"
+print(name[0])
+print(name[-1])
+print(name[2:])
+print(name[::-1])
+print(name[1::2])
+print(name[0::2]) #это если нечётное число вводить по 1 символу#
+print(name[1:4]) #если вычленять середину#
+print(name *5) #умножение имени#
+print(name[::-1] * 6) #наоборот приумножение#
